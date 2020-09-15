85db1fe0f3f1cf37441f1e934111fd23be978a59
MivleDK <grevmivlos@gmail.com>
2020-09-15 09:53:41 +0200

    target

---
+++ target/generated-sources/annotations/entities/ghy636765/RenameMe_.java
@@ -0,0 +1,16 @@
+package entities.ghy636765;
+
+import entities.RenameMe;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-15T09:43:41")
+@StaticMetamodel(RenameMe.class)
+public class RenameMe_ { 
+
+    public static volatile SingularAttribute<RenameMe, String> dummyStr1;
+    public static volatile SingularAttribute<RenameMe, String> dummyStr2;
+    public static volatile SingularAttribute<RenameMe, Long> id;
+
+}
