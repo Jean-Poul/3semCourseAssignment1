0488128fce7b18a706e96eddb7fa6c91ac342326
MivleDK <grevmivlos@gmail.com>
2020-09-15 10:12:50 +0200

    new gitignore

--- target/generated-sources/annotations/entities/xx345y657/RenameMe_.java
+++ target/generated-sources/annotations/entities/xx345y657/RenameMe_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-15T09:43:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-15T10:10:51")
 @StaticMetamodel(RenameMe.class)
 public class RenameMe_ { 
 
