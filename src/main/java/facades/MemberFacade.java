a3f25b63adc66da77d06ac1310988447a664335e
Jean-Poul <cph-jl360@cphbusiness.dk>
2020-09-16 14:47:20 +0200

    Added JPQL for getting all groupmembers

--- src/main/java/facades/MemberFacade.java
+++ src/main/java/facades/MemberFacade.java
@@ -1,27 +1,30 @@
 package facades;
 
+import dto.MemberDTO;
 import entities.Member;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import utils.EMF_Creator;
 
 /**
- * 
+ *
  * @author jplm
  */
 public class MemberFacade {
 
     private static MemberFacade instance;
     private static EntityManagerFactory emf;
-    
+
     //Private Constructor to ensure Singleton
-    private MemberFacade() {}
-    
-    
+    private MemberFacade() {
+    }
+
     /**
-     * 
+     *
      * @param _emf
      * @return an instance of this facade class.
      */
@@ -36,23 +39,36 @@ public class MemberFacade {
     private EntityManager getEntityManager() {
         return emf.createEntityManager();
     }
-    
+
     // Get number of records in Member table from database
-    public long getCount(){
+    public long getCount() {
         EntityManager em = emf.createEntityManager();
-        try{
-            long count = (long)em.createQuery("SELECT COUNT(m) FROM Member m").getSingleResult();
+        try {
+            long count = (long) em.createQuery("SELECT COUNT(m) FROM Member m").getSingleResult();
             return count;
-        }finally{  
+        } finally {
             em.close();
         }
-        
+
     }
-    
+
+    // Get all groupmembers
+    public List<MemberDTO> getAllMembers() {
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Member> query = em.createQuery("SELECT m FROM Movie m", Member.class);
+        List<Member> members = query.getResultList();
+        List<MemberDTO> memberDTOs = new ArrayList();
+        members.forEach((Member member) -> {
+            memberDTOs.add(new MemberDTO(member));
+        });
+        return memberDTOs;
+    }
+
+    // Inserting to database
     public static void main(String[] args) {
         //Create emf pointing to the dev-database
         EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
-        
+
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
