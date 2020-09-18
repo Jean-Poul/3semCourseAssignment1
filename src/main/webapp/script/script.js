b04b71cf4106cab0e685cb92475f5021c86df9d6
Amazingh0rse <therahbek@gmail.com>
2020-09-18 12:07:56 +0200

    js implemented for front table+cars

--- src/main/webapp/script/script.js
+++ src/main/webapp/script/script.js
@@ -6,7 +6,6 @@ var getAllMembers = document.getElementById("getAllMembers");
 
 // All members function (building table)
 getAllMembers.onclick = function () {
-
     let newUrl = url + "all";
 
     fetch(newUrl)
@@ -29,3 +28,26 @@ getAllMembers.onclick = function () {
             });
 
 };
+
+    let newUrl = url + "all";
+
+    fetch(newUrl)
+            .then(res => res.json()) //in flow1, just do it
+            .then(data => {
+
+                let result = " <thead> <th> Student ID </th> <th> Name </th> <th> Favorite Shows </th> </thead> <tbody>";
+                for (var i in data) {
+                    result += "<tr> <td>" + data[i].studentId + "</td> <td>" + data[i].name + "</td> <td>" + data[i].favoriteShows + "</td> </tr>";
+                }
+                result += "</tbody>";
+                document.getElementById("userTable").innerHTML = result;
+
+                // Inside this callback, and only here, the response data is available
+                console.log("data", data);
+                /* data now contains the response, converted to JavaScript
+                 Observe the output from the log-output above
+                 Now, just build your DOM changes using the data*/
+
+            });
+
+
