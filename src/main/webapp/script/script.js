125b269e983453afa9160dc9ad9b3f5dc9347d68
Jean-Poul <cph-jl360@cphbusiness.dk>
2020-09-18 09:49:26 +0200

    Finishing touches to member tables and javascript

--- src/main/webapp/script/script.js
+++ src/main/webapp/script/script.js
@@ -1,7 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-console.log("Hello");
+
+var url = "api/groupmembers/";
+var getAllMembers = document.getElementById("getAllMembers");
+
+getAllMembers.onclick = function () {
+
+    let newUrl = url + "all";
+
+    fetch(newUrl)
+            .then(res => res.json()) //in flow1, just do it
+            .then(data => {
+
+                let result = " <thead> <th> Student ID </th> <th> Name </th> <th> Favorite Shows </th> </thead> <tbody>";
+                for (var i in data) {
+                    result += "<tr> <td>" + data[i].studentId + "</td> <td>" + data[i].name + "</td> <td>" + data[i].favoriteShows + "</td> </tr>";
+                }
+                result += "</tbody>";
+                document.getElementById("userTable").innerHTML = result;
+
+                // Inside this callback, and only here, the response data is available
+                console.log("data", data);
+                /* data now contains the response, converted to JavaScript
+                 Observe the output from the log-output above
+                 Now, just build your DOM changes using the data*/
+
+            });
+
+};
